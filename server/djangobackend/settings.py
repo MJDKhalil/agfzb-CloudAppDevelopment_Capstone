2cc5cdf2cae38f59abbd09b75d77e536495da28f
MJDKhalil <moammer.khalil@outlook.com>
2021-11-25 09:25:40 +0100

    change in setting

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["moapp.eu-gb.mybluemix.net","moapp.eu-gb.cf.appdomain.cloud", "127.0.0.1"]
+ALLOWED_HOSTS = ["moapp.eu-gb.mybluemix.net", "127.0.0.1"]
 
 # Application definition
 
